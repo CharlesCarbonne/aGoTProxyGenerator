8d7c3078709ffef7465edf785edd3f07820ebab6
CharlesCarbonne <charlescarbonne@hotmail.fr>
2017-10-24 09:33:44 +0100

    Gestion des cartes complot en les saisissant dans un formulaire à part

--- js/app.js
+++ js/app.js
@@ -1,32 +1,85 @@
 $(document).ready(function() {
-  $("#Go").click(function displayProxy() {
+  $("#GetCards").click(function displayProxy() {
     $.getJSON('https://thronesdb.com/api/public/cards/', function(gotResults) {
       var lines = $('textarea').val().split('\n');
       for (var j = 0; j < lines.length; j++) {
-        var x = lines[j].split(' ')[0];
+        var z = lines[j].split(' ')[0];
+        var x = z.slice(0, -1);
+        var regExp = /\(([^)]+)\)/;
         var cardLabel = lines[j].split(' ').splice(1).join(' ');
+        var cardExtensionTab = lines[j].split(' ').splice(lines[j].split(' ').length - 1);
+        var cardExtension = regExp.exec(cardLabel)[1];
+        var lastIndex = cardLabel.lastIndexOf(" (");
+        var cardName = cardLabel.substring(0, lastIndex);
+
+
+
+
+
         //  var cardLabel = lines[j].substring(2);
         //var x = lines[j].split(0);
-        console.log(cardLabel)
-        console.log(x);
+        console.log("label complet: " + cardLabel)
+        console.log("Extension : " + cardExtension)
+        console.log("Nombre de cartes: " + x)
+        console.log("Nom de Carte : " + cardName);
+        console.log(lastIndex)
         for (var n = 0; n < x; n++) { //Pour chaque ligne
           for (var i = 0; i < gotResults.length; i++) { //Pour chaque objet du json
             if (gotResults[i].label === cardLabel) { //Si label objet === nom tapé
+                $("#img-section").html($("#img-section").html() + '<img src=https://thronesdb.com' + gotResults[i].imagesrc + ' class= "scan"></img>');
+            } else if (gotResults[i].label === cardName) {
               $("#img-section").html($("#img-section").html() + '<img src=https://thronesdb.com' + gotResults[i].imagesrc + ' class= "scan"></img>');
-            } else if (gotResults[i].name === cardLabel) {
-              $("#tablelabel").html($("#tablelabel").html() + '<tr><td id="cardName"></td><td id="labelResults"></td>');
-              $("#cardName").html(cardLabel);
-              $("#labelResults").html($("#labelResults").html() + gotResults[i].label + "<br/>");
             }
-
           }
         }
       }
     });
   });
 
+
+  $("#GetPlots").click(function displayPlots() {
+    $.getJSON('https://thronesdb.com/api/public/cards/', function(gotResults) {
+      var lines = $('#plotarea').val().split('\n');
+      for (var j = 0; j < lines.length; j++) {
+        var z = lines[j].split(' ')[0];
+        var x = z.slice(0, -1);
+        var regExp = /\(([^)]+)\)/;
+        var cardLabel = lines[j].split(' ').splice(1).join(' ');
+        var cardExtensionTab = lines[j].split(' ').splice(lines[j].split(' ').length - 1);
+        var cardExtension = regExp.exec(cardLabel)[1];
+        var lastIndex = cardLabel.lastIndexOf(" (");
+        var cardName = cardLabel.substring(0, lastIndex);
+
+
+
+
+
+        //  var cardLabel = lines[j].substring(2);
+        //var x = lines[j].split(0);
+        console.log("label complet: " + cardLabel)
+        console.log("Extension : " + cardExtension)
+        console.log("Nombre de cartes: " + x)
+        console.log("Nom de Carte : " + cardName);
+        console.log(lastIndex)
+        for (var n = 0; n < x; n++) { //Pour chaque ligne
+          for (var i = 0; i < gotResults.length; i++) { //Pour chaque objet du json
+            if (gotResults[i].label === cardLabel) { //Si label objet === nom tapé
+                $("#plot-section").html($("#plot-section").html() + '<img src=https://thronesdb.com' + gotResults[i].imagesrc + ' class= "plot"></img>');
+            } else if (gotResults[i].label === cardName) {
+              $("#plot-section").html($("#plot-section").html() + '<img src=https://thronesdb.com' + gotResults[i].imagesrc + ' class= "plot"></img>');
+            }
+          }
+        }
+      }
+    });
+  });
+
+
+
+
   $("#resetProxy").click(function() {
     $("#img-section").empty();
+    $("#plot-section").empty();
   });
 
 });
@@ -34,9 +87,3 @@ $(document).ready(function() {
 function printProxys() {
   window.print();
 }
-
-//$(document).ready(function() {
-$("#resetProxy").click(function() {
-  $("#img-section").empty();
-});
-//});
